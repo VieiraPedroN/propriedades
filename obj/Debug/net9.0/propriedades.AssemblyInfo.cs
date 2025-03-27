467836d28a29bf58a7a30fc44abd275423264d29
Pedro <pedronicolas576@gmail.com>
2025-03-26 22:53:34 -0300

    Realizando interpolação de Strings

--- obj/Debug/net9.0/propriedades.AssemblyInfo.cs
+++ obj/Debug/net9.0/propriedades.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("propriedades")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08df52d4d61826964ea0937c3a0d195cafdce9f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf9053b7ac86d3e6e2927669b1ea0014629ee82a")]
 [assembly: System.Reflection.AssemblyProductAttribute("propriedades")]
 [assembly: System.Reflection.AssemblyTitleAttribute("propriedades")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
